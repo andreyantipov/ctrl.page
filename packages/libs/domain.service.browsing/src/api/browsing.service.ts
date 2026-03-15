ca49e0bb1127821326ab4e897bad155923f60238
Developer <586380+andreyantipov@users.noreply.github.com>
2026-03-15 11:26:03 +0000

    fix: sort imports in browsing service (biome)

--- packages/libs/domain.service.browsing/src/api/browsing.service.ts
+++ packages/libs/domain.service.browsing/src/api/browsing.service.ts
@@ -1,5 +1,6 @@
 // The RPC group (BrowsingRpcs) is the canonical service contract.
 // The handler layer (BrowsingHandlersLive) is the canonical implementation.
 // The old BrowsingService Context.Tag has been removed.
-export { BrowsingRpcs } from "./browsing.rpc";
+
 export { BrowsingHandlersLive } from "./browsing.handlers";
+export { BrowsingRpcs } from "./browsing.rpc";
--- packages/libs/domain.service.browsing/src/index.ts
+++ packages/libs/domain.service.browsing/src/index.ts
@@ -1,4 +1,4 @@
-export { BrowsingRpcs } from "./api/browsing.rpc";
 export { BrowsingHandlersLive } from "./api/browsing.handlers";
+export { BrowsingRpcs } from "./api/browsing.rpc";
 export { BROWSING_SERVICE } from "./lib/constants";
 export type { BrowsingState } from "./model/browsing.events";
